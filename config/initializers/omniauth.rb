a63972ca54788535ba28e48be14c7da73059fcfa
muff1225 <graphil1225@yahoo.co.jp>
2013-10-28 01:00:58 +0900

    nandeyanenn?

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, '9jfwdvEVleTkxItJq719mQ', 'guukqq6LWvDPbTVKpcGv0qg9NWZfG3NXcJ9cKplxo'
+  #provider :twitter, '9jfwdvEVleTkxItJq719mQ', 'guukqq6LWvDPbTVKpcGv0qg9NWZfG3NXcJ9cKplxo'
+  provider :twitter, 'Phqo6RpoqCEPKJmMNKoQUQ', 'm8kuLcwD8JbNWey64lha943WkikPcHK4pI7ANrDXNA '
 end
