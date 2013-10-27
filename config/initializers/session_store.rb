807c556c4b9c5783f5dd33be2d4f287d834517c4
muff1225 <graphil1225@yahoo.co.jp>
2013-10-27 22:36:38 +0900

    fix session butubutuman

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-RailsApp::Application.config.session_store :cookie_store, key: '_rails_app_session', domain: 'cln9naf-au8-app000.c4sa.net'
+RailsApp::Application.config.session_store :cookie_store, key: '_rails_app_session', domain: '.cln9naf-au8-app000.c4sa.net'
 # RailsApp::Application.config.session_store :redis_store, :redis_server => {:host => ENV["C4SA_SHARED_REDIS_IP"], :port => ENV["C4SA_SHARED_REDIS_PORT"], :password => ENV["C4SA_SHARED_REDIS_PASSWORD"]}
 
 # Use the database for sessions instead of the cookie-based default,
