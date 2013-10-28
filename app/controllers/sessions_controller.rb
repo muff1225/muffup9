2863d967f94fc1a7c689628a66e77dc4789fc6a6
muff1225 <graphil1225@yahoo.co.jp>
2013-10-28 09:05:07 +0900

    ikenakatta

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,20 +11,20 @@ class SessionsController < ApplicationController
       client.user_timeline(session[:username], options).each do |res|
        p "#{res.from_user}: #{res.text}"
       end
-      redirect_to root_url, :notice => "認証しました"
+      redirect_to 'http://www.thebasecamp.jp', :notice => "認証しました"
   end
 
   def destroy
     set_auth_session(nil)
-    redirect_to root_url, :notice => "認証を外しました"
+    redirect_to 'http://www.thebasecamp.jp', :notice => "認証を外しました"
   end
 
   def failure
-    redirect_to root_url, :alert => "認証できませんでした"
+    redirect_to 'http://www.thebasecamp.jp', :alert => "認証できませんでした"
   end
 
   def cancel
-    redirect_to root_url, :notice => "認証途中でキャンセルしました"
+    redirect_to 'http://www.thebasecamp.jp', :notice => "認証途中でキャンセルしました"
   end
 
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :twitter, '9jfwdvEVleTkxItJq719mQ', 'guukqq6LWvDPbTVKpcGv0qg9NWZfG3NXcJ9cKplxo'
-  provider :twitter, 'Phqo6RpoqCEPKJmMNKoQUQ', 'm8kuLcwD8JbNWey64lha943WkikPcHK4pI7ANrDXNA'
+  provider :twitter, '9jfwdvEVleTkxItJq719mQ', 'guukqq6LWvDPbTVKpcGv0qg9NWZfG3NXcJ9cKplxo'
   OmniAuth.config.full_host = 'http://www.thebasecamp.jp'
+
+  #this is a test provider keys
+  #provider :twitter, 'Phqo6RpoqCEPKJmMNKoQUQ', 'm8kuLcwD8JbNWey64lha943WkikPcHK4pI7ANrDXNA'
 end
